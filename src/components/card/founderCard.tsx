7c3a6f60bb32e145049e1990ed0f278a9ba092f7
Adik Soleh <adiksoleh4@gmail.com>
2025-01-11 19:20:59 +0700

    style: fix style founderCard

--- src/components/card/founderCard.tsx
+++ src/components/card/founderCard.tsx
@@ -1,26 +1,30 @@
-import { Card, Flex, Image, Text } from "@chakra-ui/react";
-import ustad from "@/assets/images/ustad.jpg"
+import ustad from "@/assets/images/ustad.jpg";
+import { Card, CardBody, CardFooter, Flex, Heading, Image, Text } from "@chakra-ui/react";
+
 export const FounderCard = () => {
   return (
-    <Card.Root maxW="100vh" overflow="hidden"  alignItems={"center"}>
-      <Flex>
-      <Image
-      objectFit="cover"
-       maxW="50vh"
-        src={ustad}
-        alt="Green double couch with wooden legs"
-      />
+    <Card.Root maxW="400px" overflow="hidden" alignItems="center" boxShadow="md">
+      <Flex justify="center">
+        <Image
+          objectFit="cover"
+          maxW="100%" // Menyesuaikan lebar maksimal dengan Card
+          maxH="300px" // Mengatur tinggi maksimum gambar
+          src={ustad}
+          alt="Foto Pendiri"
+        />
       </Flex>
-      <Card.Body gap="2" alignItems={"center"}>
-        <Card.Title mb="2">Alm. Kh. Nurhasan Bin H.Muhammad</Card.Title>
-        <Text color={"gray.300"}>Pendiri</Text>
-        <Card.Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, commodi. Odio blanditiis vel culpa sapiente, quaerat repellendus reiciendis recusandae itaque exercitationem repellat dolorem, quae distinctio consectetur nemo, excepturi nam voluptate.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer gap="2">
-        <Text color={"green.500"}>Selengkapnya</Text>
-      </Card.Footer>
+      <CardBody textAlign="center">
+        <Heading mb="2">Alm. Kh. Nurhasan Bin H. Muhammad</Heading>
+        <Text color="gray.500" fontWeight="bold">Pendiri</Text>
+        <Text mt="2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, commodi. Odio blanditiis vel culpa sapiente, quaerat repellendus reiciendis recusandae itaque exercitationem repellat dolorem, quae distinctio consectetur nemo, excepturi nam voluptate.....
+        </Text>
+      </CardBody>
+      <CardFooter justifyContent="center">
+        <Text color="green.500" fontWeight="bold" cursor="pointer">
+          Selengkapnya
+        </Text>
+      </CardFooter>
     </Card.Root>
   );
 };
